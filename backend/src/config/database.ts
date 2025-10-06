6a856170c0b14b84a36527b93086bf03320a9b28
jagath2005 <122716425+jagath2005@users.noreply.github.com>
2025-10-06 20:07:36 +0530

    new

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/campusconnect';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://jagathvishnusv2023aids_db_user:8BSGVb3Ut0rJ2HzQ@cluster0.sgk5s43.mongodb.net/campusconnect?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
